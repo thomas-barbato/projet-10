9df15018fd687edfd365e1593f665eb92e44c73e
Case Gibson <t.barbato78@gmail.com>
2023-03-11 23:49:18 +0100

    models added

--- api/urls.py
+++ api/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views as core_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('signup', core_views.SignupView.as_view(), name='signup')
 ]
--- core/models.py
+++ core/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.urls import reverse
+import os
 
-# Create your models here.
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings")
+
+class Users(models.Model):
+    user = models.IntegerField(primary_key=True, editable=False)
+    first_name = models.CharField(max_length=500)
+    last_name = models.CharField(max_length=500)
+    email = models.EmailField(max_length=500)
+    password = models.CharField(max_length=500)
+
+class Projects(models.Model):
+    project = models.IntegerField(primary_key=True, editable=False)
+    title = models.CharField(max_length=500)
+    description = models.CharField(max_length=500)
+    type = models.CharField(max_length=500)
+    author_user = models.ForeignKey(Users, on_delete=models.RESTRICT)
+
+    def __str__(self):
+        return self.title
+
+class Issues(models.Model):
+    title = models.CharField(max_length=500)
+    desc = models.CharField(max_length=500)
+    tag = models.CharField(max_length=500)
+    priority = models.CharField(max_length=500)
+    project = models.ForeignKey(Projects, on_delete=models.RESTRICT)
+    status = models.CharField(max_length=500)
+    author_user = models.ForeignKey(Users, related_name="author", on_delete=models.RESTRICT)
+    assignee_user = models.ForeignKey(Users, related_name="assignee", on_delete=models.RESTRICT)
+    created_time = models.DateTimeField("Created Time", auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Comments(models.Model):
+    comment = models.IntegerField(primary_key=True, editable=False)
+    description = models.CharField(max_length=500)
+    author_user = models.ForeignKey(Users, on_delete=models.RESTRICT)
+    issue = models.ForeignKey(Issues, on_delete=models.RESTRICT)
+    created_time = models.DateTimeField("Created Time", auto_now_add=True)
