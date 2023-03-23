6438428eb59ffd64dc4e5afd9180f938d70c4b6c
Case Gibson <t.barbato78@gmail.com>
2023-03-23 14:59:49 +0100

    Begin view cleaning

--- api/views.py
+++ api/views.py
@@ -43,10 +43,10 @@ class MyTokenObtainPairView(TokenObtainPairView):
 
 
 class ProjectViewset(ModelViewSet):
-    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    #authentication_classes = [SessionAuthentication, BasicAuthentication]
     serializer_class = ProjectSerializer
     queryset = Projects.objects.all()
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (AllowAny,)
 
     def create(self, request, *args, **kwargs):
         print(request.data)
@@ -61,26 +61,11 @@ class ProjectViewset(ModelViewSet):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def retrieve(self, request, *args, **kwargs):
-        queryset = get_object_or_404(Projects.objects.all(), pk=11)
-        serializer = ProjectSerializer(queryset, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
     def update(self, request, pk=None):
         pass
 
     def partial_update(self, request, pk=None):
         pass
-
-    def destroy(self, request, *args, **kwargs):
-        queryset = get_object_or_404(Projects, pk=kwargs['pk'])
-        print(kwargs['pk'])
-        print(queryset)
-        Contributors.objects.filter(project_id=kwargs['pk']).delete()
-        project = Projects(**queryset)
-        project.delete()
-        return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class ContributorViewset(viewsets.ModelViewSet):
     serializer_class = ContributorSerializer
--- settings.py
+++ settings.py
@@ -65,7 +65,6 @@ REST_FRAMEWORK = {
     #),
     'DEFAULT_PERMISSION_CLASSES': (
         "rest_framework.permissions.AllowAny",
-        "rest_framework.permissions.IsAuthenticated",
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 100,
